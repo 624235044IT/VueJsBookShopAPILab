b668cc5da170ce92992700881aa86ad9c34fea50
624235044 <624235044@parichat.skru.ac.th>
2021-10-22 13:04:24 +0700

    add

--- config/db.config.js
+++ config/db.config.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const dbConn = mysql.createConnection({
   host     : 'localhost',    
   user     : 'root',
-  password : '624235044',
+  password : '',
   database : 'bookshopdb'
 });
 dbConn.connect(function(err) {
